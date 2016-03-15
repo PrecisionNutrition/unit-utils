4c8e2d6f5a0c691897f0d39ac24c638968208819
James Herdman <james.herdman@gmail.com>
2016-03-15 13:48:05 -0400

    Add unitConverter Util

---
+++ addon/utils/unit-converter.js
@@ -0,0 +1,57 @@
+import Ember from 'ember';
+
+const { isEmpty } = Ember;
+
+const SUPPORTED_CONVERSIONS = {
+  centimeters: {
+    inches(centimeters) {
+      return centimeters * 0.3937;
+    }
+  },
+
+  inches: {
+    centimeters(inches) {
+      return inches * 2.54;
+    }
+  },
+
+  meters: {
+    feet(meters) {
+      return meters * 3.281;
+    }
+  },
+
+  feet: {
+    meters(feet) {
+      return feet * 0.305;
+    }
+  },
+
+  kilograms: {
+    pounds(kilograms) {
+      return kilograms * 2.20462;
+    }
+  },
+
+  pounds: {
+    kilograms(pounds) {
+      return pounds * 0.4536;
+    }
+  }
+};
+
+export default function unitConverter({ value, fromUnit, toUnit }) {
+  if (isEmpty(value) || isEmpty(fromUnit) || isEmpty(toUnit)) {
+    return;
+  }
+
+  if (fromUnit !== toUnit) {
+    let conversions = SUPPORTED_CONVERSIONS[fromUnit];
+    let converter = conversions[toUnit];
+    let convertedValue = converter(value);
+
+    return convertedValue;
+  } else {
+    return value;
+  }
+}
