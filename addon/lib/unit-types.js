c55d04522fe91d0d5401a364c8c79e652bc746ce
James Herdman <james.herdman@gmail.com>
2017-01-11 15:12:51 -0500

    Units List is an Ember Array

--- addon/lib/unit-types.js
+++ addon/lib/unit-types.js
@@ -9,27 +9,27 @@ export default A([
     name: 'length',
     base: 'meters',
     preferenceKey: 'lengthUnits',
-    units: [
+    units: A([
       Ember.Object.create({ name: 'meters', shortName: 'm' }),
-      Ember.Object.create({ name: 'feet', shortName: 'ft' })
-    ]
+      Ember.Object.create({ name: 'feet', shortName: 'ft' }),
+    ]),
   }),
   Ember.Object.create({
     name: 'small length',
     base: 'centimeters',
     preferenceKey: 'smallLengthUnits',
-    units: [
+    units: A([
       Ember.Object.create({ name: 'centimeters', shortName: 'cm' }),
       Ember.Object.create({ name: 'inches', shortName: 'in' }),
-    ]
+    ]),
   }),
   Ember.Object.create({
     name: 'weight',
     base: 'kilograms',
     preferenceKey: 'massUnits',
-    units: [
+    units: A([
       Ember.Object.create({ name: 'kilograms', shortName: 'kg' }),
       Ember.Object.create({ name: 'pounds', shortName: 'lbs' }),
-    ]
-  })
+    ]),
+  }),
 ]);
