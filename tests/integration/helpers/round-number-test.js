72fd17b41ede96f3fa6a0a45b9f6e9dd07a9b10a
James Herdman <james.herdman@gmail.com>
2018-10-16 20:17:19 +0000

    Upgrade Ember, and Friends, to 3.5

---
+++ tests/integration/helpers/round-number-test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | round-number', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it rounds a number to a whole number if precision is 0', async function(assert) {
+    let value = 123.123123;
+    let precision = 0;
+
+    this.setProperties({
+      value,
+      precision,
+    });
+
+    await render(hbs`{{round-number value=value precision=precision}}`);
+
+    assert.dom('*').hasText('123');
+  });
+
+  test('it rounds to the specified precision', async function(assert) {
+    let value = 123.456;
+    let precision = 1;
+
+    this.setProperties({
+      value,
+      precision,
+    });
+
+    await render(hbs`{{round-number value=value precision=precision}}`);
+
+    assert.dom('*').hasText('123.5');
+  });
+});
